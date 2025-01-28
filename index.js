d46077a140e68af92d03e690e11b0feeb968a0fd
jpllopes96 <joaolacaia96@gmail.com>
2025-01-28 13:01:43 -0300

    feat: add GetUserByIdController

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config.js'
 import express from 'express'
 import { CreateUserControler } from './src/controller/create-user.js'
+import { GetUserByIdController } from './src/controller/get-user-by-id.js'
 
 const app = express()
 
@@ -10,6 +11,14 @@ app.post('/api/users', async (request, response) => {
     const createUserControler = new CreateUserControler()
 
     const { statusCode, body } = await createUserControler.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+app.get('/api/users/:userId', async (request, response) => {
+    const getUserByIdController = new GetUserByIdController()
+
+    const { statusCode, body } = await getUserByIdController.execute(request)
 
     response.status(statusCode).send(body)
 })
