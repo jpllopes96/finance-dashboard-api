67523c6495d785bce5a02deccc9719125a1dc862
jpllopes96 <joaolacaia96@gmail.com>
2025-02-06 00:08:14 -0300

    feat: add CreateTransactionUseCase

--- src/errors/users.js
+++ src/errors/users.js
@@ -4,3 +4,10 @@ export class EmailAlreadyInUserError extends Error {
         this.name = 'EmailAlreadyInUseError'
     }
 }
+
+export class UserNotFoundError extends Error {
+    constructor(userId) {
+        super(`User with id ${userId} not found.`)
+        this.name = 'UserNotFoundError'
+    }
+}
