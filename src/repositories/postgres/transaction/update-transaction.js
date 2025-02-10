1274f8ee3dd90b9a2f2bd0a600008f9495a1d25d
jpllopes96 <joaolacaia96@gmail.com>
2025-02-10 20:20:04 -0300

    fix: table name fixed from transaction to transactions

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -13,7 +13,7 @@ export class PostgresUpdateTransactionRepository {
         updateValues.push(transactionId)
 
         const updateQuery = `
-                    UPDATE transaction
+                    UPDATE transactions
                     SET ${updateFields.join(', ')}
                     WHERE id = $${updateValues.length}
                     RETURNING *
