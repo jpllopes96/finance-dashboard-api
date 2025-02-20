ec75c65aeeb113b08cd3e8d9338c9c64065b2a00
jpllopes96 <joaolacaia96@gmail.com>
2025-02-20 19:12:23 -0300

    test: ensue CreateUserController return 500 if throws

--- src/controller/user/create-user.test.js
+++ src/controller/user/create-user.test.js
@@ -181,4 +181,31 @@ describe('Create User Controller', () => {
         expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
         expect(executeSpy).toHaveBeenCalledTimes(1)
     })
+
+    it('should return 500 if CreateUserUseCase Throws', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({
+                    length: 7,
+                }),
+            },
+        }
+        //act
+        jest.spyOn(createUserController, 'execute').mockImplementationOnce(
+            () => {
+                throw new Error()
+            },
+        )
+
+        const result = await createUserController.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(500)
+    })
 })
