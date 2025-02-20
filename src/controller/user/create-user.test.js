9d1a8e1ef3fb9050cea3e895eaec4c3f11ded9f3
jpllopes96 <joaolacaia96@gmail.com>
2025-02-20 17:27:37 -0300

    test: ensure CreateUserUseCase is called with correct params

--- src/controller/user/create-user.test.js
+++ src/controller/user/create-user.test.js
@@ -27,7 +27,7 @@ describe('Create User Controller', () => {
 
         //assert
         expect(result.statusCode).toBe(201)
-        expect(result.body).toBe(httpRequest.body)
+        expect(result.body).toEqual(httpRequest.body)
     })
 
     it('should return 400 if first_name is not provided', async () => {
@@ -135,7 +135,8 @@ describe('Create User Controller', () => {
             body: {
                 first_name: 'Joao',
                 last_name: 'Lopes',
-                password: '123456',
+                email: 'joao@email.com',
+                password: '123',
             },
         }
 
@@ -145,5 +146,25 @@ describe('Create User Controller', () => {
         //accert
 
         expect(result.statusCode).toBe(400)
+    })
+
+    it('should call the CreateUserUseCase with correct params', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Joao',
+                last_name: 'Lopes',
+                email: 'joao@email.com',
+                password: '123456',
+            },
+        }
+
+        const executeSpy = jest.spyOn(createUserUseCase, 'execute')
+
+        await createUserController.execute(httpRequest)
+
+        expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
+        expect(executeSpy).toHaveBeenCalledTimes(1)
     })
 })
