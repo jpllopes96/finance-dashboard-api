f257baedfcf723fab384669a52040eb80649486c
jpllopes96 <joaolacaia96@gmail.com>
2025-02-25 21:11:07 -0300

    chore: add swagger initial setup

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import 'dotenv/config.js'
 import express from 'express'
-
 import { usersRouter, transactionRouter } from './src/routes/index.js'
+import swaggerUi from 'swagger-ui-express'
+import fs from 'fs'
+import path from 'path'
 
 const app = express()
 
@@ -10,6 +12,15 @@ app.use(express.json())
 app.use('/api/users', usersRouter)
 
 app.use('/api/transactions', transactionRouter)
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(
+        path.join(import.meta.dirname, './docs/swagger.json'),
+        'utf-8',
+    ),
+)
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(process.env.PORT, () =>
     console.log(`listening on port ${process.env.PORT}`),
