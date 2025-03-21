1a8bc81b6698704e680120bcc96c1dfd675afa7f
jpllopes96 <joaolacaia96@gmail.com>
2025-03-21 19:58:34 -0300

    feat: add LoginUserUseCase

---
+++ src/use-cases/user/login-user.js
@@ -0,0 +1,40 @@
+import { UserNotFoundError, InvalidPasswordError } from '../../errors/users.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+export class loginUserUseCase {
+    constructor(getUserByEmailRepository) {
+        this.getUserByEmailRepository = getUserByEmailRepository
+    }
+
+    async execute(email, password) {
+        const user = await this.getUserByEmailRepository.execute(email)
+
+        if (!user) {
+            throw new UserNotFoundError()
+        }
+
+        const isPasswordValid = bcrypt.compare(password, user.password)
+        if (!isPasswordValid) {
+            throw new InvalidPasswordError()
+        }
+
+        const tokens = {
+            accessToken: jwt.sign(
+                { userId: user.id },
+                process.env.JWT_ACCESS_TOKEN_SECRET,
+                { expiresIn: '15m' },
+            ),
+
+            refreshToken: jwt.sign(
+                { userId: user.id },
+                process.env.JWT_REFRESH_TOKEN_SECRET,
+                { expiresIn: '30d' },
+            ),
+        }
+
+        return {
+            ...user,
+            tokens,
+        }
+    }
+}
